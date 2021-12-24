090d5217851c8314685dc35160e3f1e7714d9acf
banevare <30812501+banevare@users.noreply.github.com>
2021-12-23 17:13:49 -0800

    javascript solution

---
+++ topKFrequent/topKFrequent.js
@@ -0,0 +1,16 @@
+var topKFrequent = function(nums,k) {
+    var dict = {};
+    for(var n of nums) dict[n] = 0;
+    for(var n of nums) dict[n]++;
+    var items = Object.keys(dict).map(function(key) {
+        return [key, dict[key]];
+    });
+    items.sort(function(first, second) {
+     return second[1] - first[1];
+    });
+    var a = [];
+    for(var i = 0; i < k; i++){
+        a[i] = items[i][0];
+    }
+    return a;
+}
