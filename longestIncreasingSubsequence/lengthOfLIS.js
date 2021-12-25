37149fa5ada4c4be2a703384cd6877cedec1d6d4
banevare <30812501+banevare@users.noreply.github.com>
2021-12-25 13:17:11 -0800

    Add files via upload

---
+++ longestIncreasingSubsequence/lengthOfLIS.js
@@ -0,0 +1,16 @@
+var lengthOfLIS = function(nums) {
+    var LIS = new Array();  
+    for(var i= 0; i < nums.length; i++){                                    // initialize every entry as 1 
+        LIS[i] = 1;
+    }
+    var maxVal = 1;
+    for(var j = nums.length - 1; j >= 0; j--){                              // traverse from end to beginning using         
+        for (var k = j - 1; k >= 0; k--){                                   // previous values to update LIS list 
+            if(nums[k] < nums[j]){
+                LIS[k] = Math.max(LIS[k],1 + LIS[j]);
+                if (LIS[k] > maxVal) maxVal = LIS[k];
+            }
+        }
+    }
+    return maxVal;                                                          // returns maximum # of subsequence
+};
