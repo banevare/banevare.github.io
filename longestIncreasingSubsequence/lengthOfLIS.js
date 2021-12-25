ff7667ec56f7afae3bb5ff74ba0b4ce9a5b9d11b
banevare <30812501+banevare@users.noreply.github.com>
2021-12-25 13:38:08 -0800

    Update lengthOfLIS.js

--- longestIncreasingSubsequence/lengthOfLIS.js
+++ longestIncreasingSubsequence/lengthOfLIS.js
@@ -1,8 +1,6 @@
 var lengthOfLIS = function(nums) {
     var LIS = new Array();  
-    for(var i= 0; i < nums.length; i++){                                    // initialize every entry as 1 
-        LIS[i] = 1;
-    }
+    for(var i= 0; i < nums.length; i++) LIS[i] = 1;                         // initialize every entry as 1 
     var maxVal = 1;
     for(var j = nums.length - 1; j >= 0; j--){                              // traverse from end to beginning using         
         for (var k = j - 1; k >= 0; k--){                                   // previous values to update LIS list 
